5eaee6fb5c17d4b1d4a02632b6ae57b5aede067a
hamzamak <makhkhas.hamza@gmail.com>
2023-01-31 11:38:22 +0100

    change baseUrl after deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const API = axios.create({baseURL: 'http://localhost:5000'})
+                                    // http://localhost:5000 for development
+const API = axios.create({baseURL: 'https://memory-3mau.onrender.com'})
 
 API.interceptors.request.use((req)=> {
     if(localStorage.getItem('profile'))  {
